2d2ca7d672ea3164b5b4c5e16e61d9f13fbbb902
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-03 15:16:14 +0530

    Area of the triangle

--- AreaOfTriangle.cpp
+++ AreaOfTriangle.cpp
@@ -5,6 +5,6 @@ int main(){
 	cout<<"Enter the lengths of the sides of the triangle";
 	cin>>s1>>s2>>s3;
 	s=(s1+s2+s3)/2;
-	cout<<"Area of the triangle:"<<pow((s*(s-s1)*(s-s2)*(s-s3)),0.5);
+	cout<<"Area of the triangle:"<<sqrt(s*(s-s1)*(s-s2)*(s-s3));
 	return 0;
 }
