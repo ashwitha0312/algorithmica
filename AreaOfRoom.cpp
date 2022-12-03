363bb45b7d21c3e653e1eccf9a27e37fb57ddc69
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-03 15:00:24 +0530

    Area of the room

---
+++ AreaOfRoom.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	ll w, l;
+	cout<<"Enter width and length of the room in centimetres:";
+	cin>>w>>l;
+	cout<<"Area of the room in metres :"<<(w*l)/100<<"square metres";
+	return 0;
+}
