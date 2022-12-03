d8f9493eedc7b9be5c01ec7ab2a4cead9a70403b
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-03 15:21:08 +0530

    Add files via upload

---
+++ Expressions/AreaOfTriangle.cpp
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int s1, s2, s3,s;
+	cout<<"Enter the lengths of the sides of the triangle";
+	cin>>s1>>s2>>s3;
+	s=(s1+s2+s3)/2;
+	cout<<"Area of the triangle:"<<sqrt(s*(s-s1)*(s-s2)*(s-s3));
+	return 0;
+}
+=======
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int s1, s2, s3,s;
+	cout<<"Enter the lengths of the sides of the triangle";
+	cin>>s1>>s2>>s3;
+	s=(s1+s2+s3)/2;
+	cout<<"Area of the triangle:"<<pow((s*(s-s1)*(s-s2)*(s-s3)),0.5);
+	return 0;
+}
+>>>>>>> 29f5906ea18f7da7e7f64fdfcceeb0aa256e8912
