cd6024eaf1cdcde3f4bf7000ead6b9641bde8b94
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-03 16:06:07 +0530

    Create BottleDeposits.cpp

---
+++ Expressions/BottleDeposits.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int x, y;
+	cout<<"Enter the no.of containers of atmost and more than 1 litre: ";
+	cin>>x>>y;
+	cout<<x*0.10+y*0.25;
+	return 0;
+}
+
