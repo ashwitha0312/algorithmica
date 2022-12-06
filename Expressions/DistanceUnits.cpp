c9e04e57ae40fdc3b8ebd67e9fc6c6498ca11e8b
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-06 15:04:37 +0530

    Create DistanceUnits.cpp

---
+++ Expressions/DistanceUnits.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	float x;
+	cout<<"Enter the measurement in feets: ";
+	cin>>x;
+	cout<<12*x<<" inches"<<"\n";
+	cout<<x/3<<" yards"<<"\n";
+	cout<<x/5280<<" miles"<<"\n";
+	return 0;
+}
