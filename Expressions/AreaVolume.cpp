b4efe51271ae9656bb5abc064e2588d26e1f1f9c
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-06 15:41:30 +0530

    Create AreaVolume.cpp

---
+++ Expressions/AreaVolume.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	float r;
+	cout<<"Enter the radius: ";
+	cin>>r;
+	cout<<"Area of circle: "<<3.14*r*r<<"\n"<<"Volume of sphere: "<<(4/3)*3.14*r*r*r;
+	return 0;
+}
