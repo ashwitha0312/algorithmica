4dc975e6be90d6c280516578d5264cbecb9d4b40
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-06 15:52:23 +0530

    Create EvenOdd.cpp

---
+++ Decision making/EvenOdd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+	ll n;
+	cout<<"Enter an integer: ";
+	cin>>n;
+	if(n%2 == 0)
+		cout<<"Even";
+	else
+		cout<<"Odd";
+	return 0;
+}
