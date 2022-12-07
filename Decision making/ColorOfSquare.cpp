ebce2d6dcd1379ed422f298b3553b183fc78b630
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-07 16:16:10 +0530

    Update ColorOfSquare.cpp

--- Decision making/ColorOfSquare.cpp
+++ Decision making/ColorOfSquare.cpp
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+	string pos;
 	char c;
-	int i;
+	int r;
 	cout<<"Enter the position: ";
-	cin>>c>>i;
+	cin>>pos;
+	c=pos[0];
+	r=pos[1];
 	if(c=='a' || c=='c' || c=='e' || c=='g'){
-		if(i%2 == 1)
+		if(r%2 == 1)
 			cout<<"square is black";
 		else
 			cout<<"square is white";
 	}
 	else{
-		if(i%2 == 1)
+		if(r%2 == 1)
 			cout<<"white";
 		else
 			cout<<"black";
