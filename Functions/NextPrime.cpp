28e400c775c235e1d3103f9b15c16947e59c529b
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-16 09:01:53 +0530

    Prime

--- Functions/NextPrime.cpp
+++ Functions/NextPrime.cpp
@@ -1,19 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isPrime(int n){
-	if(n<=1)
-		return false;
+int nextPrime(int n){
+	n+=1;
 	for(int i=2;i<n;i++){
 		if(n%i==0)
-			return false;
+			n++;
 	}
-	return true;
+	return n;
 	
 }
 int main(){
 	int n;
 	cout<<"Enter a number: ";
 	cin>>n;
-	cout<<isPrime(n);
+	cout<<nextPrime(n);
 	return 0;
 }
