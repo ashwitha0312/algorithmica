06af0bc4e4943d627424ad0442a145ab45008625
ashwitha0312 <119606356+ashwitha0312@users.noreply.github.com>
2022-12-19 16:21:16 +0530

    Update CollinearPoints.cpp

--- Functions/CollinearPoints.cpp
+++ Functions/CollinearPoints.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-string isCollinear(int x1, int y1, int x2, int y2, int x3, int y3){
+bool isCollinear(int x1, int y1, int x2, int y2, int x3, int y3){
 	float m,n;
 	m=float(y2-y1)/(x2-x1);
 	n=float(y3-y2)/(x3-x2);
 	
 	if(m==n)
-		return "true";
+		return true;
 	else
-		return "false";
+		return false;
 }
 int main(){
 	int x1,y1,x2,y2,x3,y3;
